c71854d7025c68a9a8cb7e14916fb7144e1c3dca
knowsarzehmeh <osarzeh@gmail.com>
2024-06-10 14:34:38 +0100

    completed the dice app challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  final int MAX_VALUE = 6;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(MAX_VALUE) + 1;
+      rightDiceNumber = Random().nextInt(MAX_VALUE) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice${leftDiceNumber}.png'),
+              onPressed: rollDice,
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: rollDice,
+                child: Image.asset('images/dice${rightDiceNumber}.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
